8081167572bdf9d376b1de43e5ca99120b3814a3
Sekhar Routray <sekharroutray@gmail.com>
2020-07-21 15:19:58 +0530

    added a few other keys

--- src/index.js
+++ src/index.js
@@ -26,3 +26,7 @@ var API_KEY = "sdfasdf";
 var access_key = "sdfsdf";
 var AWS_SECRET_ACCESS_KEY = "TestKeyPassPwd";
 var DSA_PRIVATE_KEY = "sdkflasaskdjfhjksdhfkdsjf";
+var git_token = "$GIT_TOKEN";
+var gpg_keys = "***REMOVED***";
+var keys = "4dsf245dsf896ds8f59ds65f4fdf587F5ds8dsf95ds87f";
+var ssh_keys = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MWRjMA=="
